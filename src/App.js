138749d2c8938ec07218c8f69c0ef29bb12560a2
Rishi kalra <rishi@pc-2-146.customer.ask4.lan>
2023-10-07 13:56:15 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="grey-header">MakaStory</h1>
+        <button className="button">Start Adventure</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
