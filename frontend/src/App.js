53980c65f671594b875ba7dbf114e7549d527d9b
Ji Chen, Rui Han (k20027110) <ruihan.ji@kcl.ac.uk>
2023-10-07 13:57:33 +0100

    Flask back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <h1>React and flask</h1>
+            {/* Calling a data from setdata for showing */}
+            <p>{data.name}</p>
+            <p>{data.age}</p>
+            <p>{data.date}</p>
+            <p>{data.programming}</p>
+
+        </header>
     </div>
-  );
+);
 }
 
 export default App;
